0393e7e59da26875617b4e00e83665583535030d
Mara Just <marajust@mjust03-MBP.fritz.box>
2021-04-26 09:03:25 +0200

    add new script

---
+++ Assets/MyGame/Scripts/ToggleAndButtons.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleAndButtons : MonoBehaviour
+{
+    public Toggle Toggle1;
+    public Toggle Toggle2;
+
+    public Text Toggle1Text;
+    public Text Toggle2Text;
+
+    public Button Und;
+    public Button Oder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckToggles ()
+    {
+
+    }
+
+    public void UndButt ()
+    {
+
+    }
+
+    public void OdButt ()
+    {
+
+    }
+
+    public void ChangeLabels ()
+    {
+
+    }
+}
