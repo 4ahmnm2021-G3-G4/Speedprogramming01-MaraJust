9566ca79b1413934bae1d8b963ad2702be83b721
Mara Just <marajust@mjust03-MBP.fritz.box>
2021-04-26 08:43:33 +0200

    add Assets and ProjektSettings and prepare Scenes

---
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddNumbers : MonoBehaviour
+{
+    public int NumberA;
+    public int NumberB;
+
+    public Text resultText;
+    public InputField inputA;
+    public InputField inputB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NumberA = int.Parse(inputA.text);
+        NumberB = int.Parse(inputB.text);
+    }
+
+    public void CalculateNumbers ()
+    {
+        var result = NumberA + NumberB;
+        resultText.text = "= " + result.ToString();
+        
+    }
+}
